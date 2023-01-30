3d97754b90db4b885a16a4f36a86412bf7ec482c
NelsonPhilippe <nenelphinel59@gmail.com>
2023-01-30 15:35:44 +0100

    feat(password_reset) : add routing and update with new password in db

--- src/back/src/users/user.controller.ts
+++ src/back/src/users/user.controller.ts
@@ -1,18 +1,27 @@
-import { Request, Controller, Post, UseGuards, Get, Body } from '@nestjs/common';
+import {
+  Request,
+  Controller,
+  Post,
+  UseGuards,
+  Get,
+  Body,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import * as dcrypt from 'bcrypt';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly usesrService: UsersService) {}
+  constructor(private readonly usersProvider: UsersService) {}
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
+    console.log(req.user);
     return req.user;
   }
 
-
-
+  @Post('reset_password')
+  resetPassword(@Body() resetPasswordRequestDto: ResetPasswordDto) {
+    return this.usersProvider.resetPassword(resetPasswordRequestDto);
+  }
 }
--- src/back/src/users/users.service.ts
+++ src/back/src/users/users.service.ts
@@ -2,15 +2,48 @@ import { Body, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Prisma, user } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 
+export type User = any;
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async user(userWereUniqueInput: Prisma.userWhereInput): Promise<user | null> {
+  async user(userWereInput: Prisma.userWhereInput): Promise<user | null> {
     return this.prisma.user.findFirst({
-      where: userWereUniqueInput,
+      where: userWereInput,
     });
+  }
+
+  async update(params: {
+    where: Prisma.userWhereUniqueInput;
+    data: Prisma.userUpdateInput;
+  }): Promise<user> {
+    const { where, data } = params;
+    return this.prisma.user.update({ data, where });
+  }
+
+  async resetPassword(resetPasswordDto: ResetPasswordDto) {
+    const user = await this.user({ mail: resetPasswordDto.mail });
+    if (user) {
+      await this.update({
+        data: {
+          password: resetPasswordDto.new_password,
+        },
+        where: {
+          id: user.id,
+        },
+      });
+
+      return JSON.parse(
+        JSON.stringify({
+          statusCode: 200,
+          description: 'Mot de passe mis à jour',
+        }),
+      );
+    }
+
+    throw new HttpException('Identifiant incorrecte', 401);
   }
 
   async findOne(mail: string): Promise<user | undefined> {
