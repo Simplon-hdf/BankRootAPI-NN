9e6b47be5135a99d0bae0ffa1927db975c9798d8
nathancuvelier <nathancuvelier59@icloud.com>
2023-01-30 11:44:07 +0100

    feat(commons):add file , add service and .dto

---
+++ src/back/src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+
+    name: string;
+    lastname: string;
+    mail: string;
+    password: string;
+    uuid: string;
+    created_at: Date;
+    update_at: Date;
+}
---
+++ src/back/src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: number;
+    name: string;
+    lastname: string;
+    mail: string;
+    password: string;
+    uuid: string;
+    created_at: Date;
+    update_at: Date;
+}
