1ac4c16530659f610c11440859a905c15c6f50b0
NelsonPhilippe <philippenelson59@gmail.com>
2023-02-01 17:17:18 +0100

    feat(transaction) : add transaction resource

--- src/back/src/transaction/dto/create-transaction.dto.ts
+++ src/back/src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTransactionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTransactionDto {
+  @ApiProperty({
+    description: 'Amount of transaction',
+  })
+  amount: number;
+  @ApiProperty({
+    description: 'State of transaction',
+  })
+  state: boolean;
+}
