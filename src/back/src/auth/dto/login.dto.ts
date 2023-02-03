9dadd93f0547f2070b9c2295983876d29d867cb2
nathancuvelier <nathancuvelier59@icloud.com>
2023-02-03 11:18:18 +0100

    build(docker) : image fix

--- src/back/src/auth/dto/login.dto.ts
+++ src/back/src/auth/dto/login.dto.ts
@@ -1,14 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
 export class LoginDto {
-  @IsString()
   @ApiProperty({
     name: 'mail',
     description: 'Mail of user for connexion',
     minimum: 1,
   })
   mail: string;
-  @IsString()
   @ApiProperty({
     name: 'password',
     description: 'Password of user for connexion',
