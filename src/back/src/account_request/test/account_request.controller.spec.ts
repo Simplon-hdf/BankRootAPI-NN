49a8c53a1fd482c9adc147223dec545d48173c7d
NelsonPhilippe <nenelphinel59@gmail.com>
2023-02-09 17:15:43 +0100

    test(test) : add unit test for user

---
+++ src/back/src/account_request/test/account_request.controller.spec.ts
@@ -0,0 +1,12 @@
+import { AccountRequestService } from '../account_request.service';
+
+describe('Test user service', () => {
+
+  let accountService
+
+  describe('#getRequestsByUser()', () => {
+    it('should return an user object', () => {
+      const result =,
+    });
+  });
+});
