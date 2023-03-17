4e8d1e2297985d4fcb222a6d1c31f1f789094340
NelsonPhilippe <philippenelson59@gmail.com>
2023-03-17 21:04:56 +0100

    docs(swagger) : add description of account request requests

--- src/back/src/account_request/account_request.controller.ts
+++ src/back/src/account_request/account_request.controller.ts
@@ -9,18 +9,27 @@ import {
 } from '@nestjs/common';
 import { AccountRequestService } from './account_request.service';
 import { CreateAccountRequestDto } from './dto/create-account_request.dto';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {ApiBody, ApiParam, ApiResponse, ApiTags} from '@nestjs/swagger';
 
 @Controller('account-request')
 @ApiTags('Account Requests Management')
 export class AccountRequestController {
   constructor(private readonly accountRequestService: AccountRequestService) {}
 
+
+  @Post()
   @ApiResponse({
     status: HttpStatus.OK,
     description: 'Request successfully created',
   })
-  @Post()
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Already request incoming',
+  })
+  @ApiBody({
+    type: CreateAccountRequestDto,
+    description: 'Create a new account request',
+  })
   createRequest(@Body() createAccountRequestDto: CreateAccountRequestDto) {
     return this.accountRequestService.createRequest(createAccountRequestDto);
   }
@@ -31,10 +40,18 @@ export class AccountRequestController {
   }
 
   @Get(':uuid')
+  @ApiParam({
+    description: 'Uuid of the user',
+    name: 'uuid',
+  })
   getAllRequestsByUser(@Param('uuid') uuid: string) {
     return this.accountRequestService.getRequestsByUser(uuid);
   }
   @Delete(':id')
+  @ApiParam({
+    description: 'Id of the request',
+    name: 'id',
+  })
   deleteRequest(@Param('id') id: number) {
     return this.accountRequestService.deleteRequest(id);
   }
