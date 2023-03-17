b53d7ac6509bce603b4f87b842a2c357de49971c
NelsonPhilippe <philippenelson59@gmail.com>
2023-03-17 21:00:29 +0100

    docs(swagger) : add description of transactions requests

--- src/back/src/transaction/dto/transaction-by-bankaccount.dto.ts
+++ src/back/src/transaction/dto/transaction-by-bankaccount.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class TransactionByBankaccountDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The bank account number',
+    example: '123456789',
+    type: 'string',
+  })
   account_num: string;
 }
