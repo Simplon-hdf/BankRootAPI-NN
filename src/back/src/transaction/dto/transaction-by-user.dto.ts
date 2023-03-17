8d06123d15865d7eec57038cb2a1aeab24290149
NelsonPhilippe <philippenelson59@gmail.com>
2023-03-17 21:00:44 +0100

    docs(swagger) : add description of transactions requests

--- src/back/src/transaction/dto/transaction-by-user.dto.ts
+++ src/back/src/transaction/dto/transaction-by-user.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class TransactionByUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The user uuid',
+  })
   user_uuid: string;
 }
