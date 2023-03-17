0de4eda0a9d2c29500d3450c3f48119eaff7d696
NelsonPhilippe <philippenelson59@gmail.com>
2023-03-17 20:59:48 +0100

    docs(swagger) : add description of transactions requests

--- src/back/src/transaction/transaction.controller.ts
+++ src/back/src/transaction/transaction.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body, Get, Param, Delete } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags} from '@nestjs/swagger';
 import { TransactionByUserDto } from './dto/transaction-by-user.dto';
 import { TransactionByBankaccountDto } from './dto/transaction-by-bankaccount.dto';
 
@@ -10,7 +10,12 @@ import { TransactionByBankaccountDto } from './dto/transaction-by-bankaccount.dt
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
+
   @Post()
+  @ApiBody({
+    description: 'The transaction to create',
+    type: CreateTransactionDto,
+  })
   create(@Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionService.createTransaction(createTransactionDto);
   }
@@ -21,6 +26,14 @@ export class TransactionController {
   }
 
   @Get('user/:uuid')
+  @ApiBody({
+    description: 'The transaction to create',
+    type: TransactionByUserDto,
+  })
+  @ApiParam({
+    name: 'uuid',
+    description: 'The user uuid',
+  })
   fetchByUser(
     @Param('uuid') uuid: string,
     @Body() transactionByUserDto: TransactionByUserDto,
@@ -29,6 +42,14 @@ export class TransactionController {
   }
 
   @Get('bank-account/:num_account')
+  @ApiParam({
+    name: 'num_account',
+    description: 'The bank account number',
+  })
+  @ApiBody({
+    description: 'The transaction to create',
+    type: TransactionByBankaccountDto,
+  })
   fetchByBankAccount(
     @Param('num_account') num_account: bigint,
     transactionByBankAccount: TransactionByBankaccountDto,
@@ -36,6 +57,10 @@ export class TransactionController {
     return this.transactionService.fetchByBankAccount(transactionByBankAccount);
   }
   @Delete(':id')
+  @ApiParam({
+    name: 'id',
+    description: 'The transaction id',
+  })
   delete(@Param('id') id: number) {
     return this.transactionService.deleteTransaction(id);
   }
