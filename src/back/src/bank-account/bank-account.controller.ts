86b75e02a3dd1c0d4c92b5aa5eae81d9e559878a
NelsonPhilippe <philippenelson59@gmail.com>
2023-03-17 21:02:49 +0100

    docs(swagger) : add description of bank account requests

--- src/back/src/bank-account/bank-account.controller.ts
+++ src/back/src/bank-account/bank-account.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, Get, Delete, Param } from '@nestjs/common';
 import { BankAccountService } from './bank-account.service';
 import { UpdateCeilingDto } from './dto/update-ceiling.dto';
 import { CreateWithdrawalDto } from './dto/Create-withdrawal.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags} from '@nestjs/swagger';
 
 @Controller('bank-account')
 @ApiTags('Bank Accounts')
@@ -10,15 +10,31 @@ export class BankAccountController {
   constructor(private readonly bankAccountService: BankAccountService) {}
 
   @Post('/update-ceiling')
+  @ApiBody({
+    type: UpdateCeilingDto,
+    description: 'Update ceiling of a bank account',
+  })
   updateCeiling(@Body() updateCeilingDto: UpdateCeilingDto) {
     return this.bankAccountService.updateCeiling(updateCeilingDto);
   }
   @Get('/:id')
+  @ApiParam({
+    description: 'Id of the bank account',
+    name: 'id',
+  })
   findOne(id: number) {
     return this.bankAccountService.findOne(id);
   }
   //retrait
   @Post(':id/withdrawal')
+  @ApiBody({
+    type: CreateWithdrawalDto,
+    description: 'Withdrawal from a bank account',
+  })
+  @ApiParam({
+    description: 'Id of the bank account',
+    name: 'id',
+  })
   withdrawal(
     @Param('id') id: number,
     @Body() createWithdrawalDto: CreateWithdrawalDto,
@@ -27,6 +43,14 @@ export class BankAccountController {
   }
   //depot
   @Post(':id/deposit')
+  @ApiBody({
+    type: CreateWithdrawalDto,
+    description: 'Deposit to a bank account',
+  })
+  @ApiParam({
+    description: 'Id of the bank account',
+    name: 'id',
+  })
   deposit(
     @Param('id') id: number,
     @Body() createWithdrawalDto: CreateWithdrawalDto,
@@ -34,6 +58,10 @@ export class BankAccountController {
     return this.bankAccountService.deposit(createWithdrawalDto);
   }
   @Delete(':id')
+  @ApiParam({
+    description: 'Id of the bank account',
+    name: 'id',
+  })
   delete(@Param('id') id: number) {
     return this.bankAccountService.delete(id);
   }
